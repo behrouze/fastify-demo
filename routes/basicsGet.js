4a1562e1de5293ddc3678aed2b027191a6cbf01b
Behrouze <dev@behrouze.com>
2020-10-23 15:54:48 +0200

    retourner des Status Codes et façonner le reply

--- routes/basicsGet.js
+++ routes/basicsGet.js
@@ -31,6 +31,32 @@ async function routes(fastify, options) {
       isAdult: request.query.age >= 18,
     };
   });
+
+  fastify.get('/error-mgmt', opts, async (request, reply) => {
+    Object.keys(request.query).length == 0
+      ? reply.status(201).send({})
+      : reply
+          .header('Content-Type', 'application/json; charset=utf-8')
+          .send({
+            hello: 'The World is Async',
+            query: request.query.toto,
+            autre: 'ne devrait pas etre sérialisé',
+            isAdult: request.query.age >= 18,
+          })
+          .then(fullfilled, rejected);
+  });
+
+  fastify.get('/throw-err', async (request, reply) => {
+    if (Object.keys(request.query).length > 0)
+      return {
+        params: request.query,
+      };
+    else
+      throw {
+        statusCode: 404,
+        message: 'aucunes data, car manque query param',
+      };
+  });
 }
 
 module.exports = routes;
